c8af87b1dfdeaee2ffd540c68f423a53c8a2084a
Chad Edwards <chadedwardsofficial@gmail.com>
2024-02-19 17:29:38 -0600

    added pages with a landingpage and also created jsx files for components like Signup etc.

---
+++ client/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  // Specify the paths to all of the files in your project
+  purge: [
+    'index.html',
+    'main.jsx',
+    // Add more paths as needed
+  ],
+
+  // Other configuration options...
+  theme: {
+    extend: {
+      // You can extend the default theme here
+    },
+  },
+  variants: {},
+  plugins: [
+    // You can add plugins here
+  ],
+}
