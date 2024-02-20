e0a7bf5376c6676fab146bd5ad7f0a36bb8097e7
lindsayagreen <lindsgree@gmail.com>
2024-02-19 20:34:11 -0600

    added footer and friends list bones

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,31 @@
+// Footer.js
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="">
+      <div className="">
+        <div>
+          <a
+            href="https://github.com/FractalIceCream/SkySocial.git" //link to github project 
+            className=""
+          >
+            Visit our github
+          </a>
+        </div>
+        <div className="">
+          <a
+            href="" //link to api website
+            className=""
+          >
+            <i className=""></i> 
+          </a> 
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
+
+//icon for api website  
