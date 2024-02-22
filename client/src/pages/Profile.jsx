c83a3a91b7e7ebd9952c9c24315610a410f1bff9
Chad Edwards <chadedwardsofficial@gmail.com>
2024-02-22 01:54:22 -0600

    added PostContainer component, added submitPost component and added a Post component

---
+++ client/src/pages/Profile.jsx
@@ -0,0 +1,35 @@
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import PostContainer from "../components/Posts/PostContainer";
+import Friends from "../components/Friends";
+import Actions from "../components/Actions"
+
+
+
+
+const Profile = () => {
+
+
+
+
+
+
+
+  return (
+    <div>
+    <Navbar />
+    <PostContainer />
+    <Friends />
+    <Actions />
+    <Footer />
+
+
+
+    </div>
+
+  )
+
+
+
+
+}
