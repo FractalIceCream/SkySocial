b40c8c7f16d583113c80c37c9536fccaba992bbb
Chad Edwards <chadedwardsofficial@gmail.com>
2024-02-23 14:52:46 -0600

    added css styling to get rid of scroll bars on the postcontainer component

--- client/src/components/Navbar/SignUpForm.jsx
+++ client/src/components/Navbar/SignUpForm.jsx
@@ -45,18 +45,18 @@ const SignUpForm = () => {
 
   return (
     <>
-      <form className="space-y-4" onSubmit={handleFormSubmit}>
+      <form className="space-y-4 " onSubmit={handleFormSubmit}>
         {/* <div
         id="authentication-modal"
         tabindex="-1"
         aria-hidden="true"
         className=" overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
       > */}
-        <div>
-          <div className="relative p-4 w-full max-w-md max-h-full">
+        <div className="">
+          <div className="relative  p-4 w-full max-w-md max-h-full">
             <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
               <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-                <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                <h3 className="text-xl font-semibold text-black dark:text-white">
                   Join the Cloud!
                 </h3>
                 {/* <button
@@ -87,7 +87,7 @@ const SignUpForm = () => {
                 <div>
                   <label
                     htmlFor="name"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    className="block mb-2 text-sm font-medium text-black dark:text-white"
                   >
                     Your Name
                   </label>
@@ -96,7 +96,7 @@ const SignUpForm = () => {
                     onChange={handleInputChange}
                     name="name"
                     id="name"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                    className="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
                     placeholder="name@company.com"
                     required
                   />
@@ -104,7 +104,7 @@ const SignUpForm = () => {
                 <div>
                   <label
                     htmlFor="email"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    className="block mb-2 text-sm font-medium text-black dark:text-white"
                   >
                     Your Email
                   </label>
@@ -113,7 +113,7 @@ const SignUpForm = () => {
                     onChange={handleInputChange}
                     name="email"
                     id="email"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                    className="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
                     placeholder="name@company.com"
                     required
                   />
@@ -121,7 +121,7 @@ const SignUpForm = () => {
                 <div>
                   <label
                     htmlFor="password"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    className="block mb-2 text-sm font-medium text-black dark:text-white"
                   >
                     Your password
                   </label>
@@ -131,7 +131,7 @@ const SignUpForm = () => {
                     name="password"
                     id="password"
                     placeholder="••••••••"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                    className="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
                     required
                   />
                 </div>
