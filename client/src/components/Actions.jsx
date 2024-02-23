85105afdaf6808ad6f415d68c484a2cef7608f1e
Terrence McCarthy <terrencemccarthysnkrs@gmail.com>
2024-02-22 22:22:43 -0600

    added auth to actions

--- client/src/components/Actions.jsx
+++ client/src/components/Actions.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom'; 
+import { Link } from 'react-router-dom';
 
 import Auth from '../utils/auth';
 
@@ -8,25 +8,22 @@ const Actions = () => {
 
 
 
-return (
+  return (
+    <>
+      {Auth.loggedIn() && ( 
+      <div class="h-actionsWidget w-actionsWidget shadow-inner-strongest font-custom box-border flex flex-wrap items-center justify-center rounded-custom bg-gray">
+        <div class="flex flex-col text-5xl font-thin text-white">
+          <button class=""><Link to='/Home'>Home</Link></button>
+          <button class="mt-9"><Link to='/Profile'>Profile</Link></button>
+          <button class="mt-9" onClick={Auth.logout}>Log Out</button>
+        </div>
+      </div>
+     )}
 
 
-<div class="h-actionsWidget w-actionsWidget shadow-inner-strongest font-custom box-border flex flex-wrap items-center justify-center rounded-custom bg-gray">
-    <div class="flex flex-col text-5xl font-thin text-white">
-      <button class="">Home</button>
-      <button class="mt-9">Profile</button>
-      <button class="mt-9">Log Out</button>
-    </div>
-  </div>
-
-
-
-
-)
-
-
-
-}
+    </>
+  );
+};
 
 
 
--- client/src/components/Navbar/SignUpForm.jsx
+++ client/src/components/Navbar/SignUpForm.jsx
@@ -17,7 +17,7 @@ const SignUpForm = () => {
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-    setProfileFormData({ ...ProfileFormData, [name]: value });
+    setProfileFormData({ ...profileFormData, [name]: value });
   };
 
   const handleFormSubmit = async (event) => {
@@ -26,6 +26,8 @@ const SignUpForm = () => {
       const { data } = await addProfile({
         variables: profileFormData,
       });
+
+      console.log(data)
 
       Auth.login(data.addProfile.token);
     } catch (err) {
@@ -131,7 +133,7 @@ const SignUpForm = () => {
                     required
                   />
                 </div>
-                <button
+                <button onSubmit={handleFormSubmit}
                   type="submit"
                   class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
