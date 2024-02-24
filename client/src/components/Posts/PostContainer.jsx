96e8a9f96a71d6cb68bb7fcb95522ac49fa18bf6
nfailor <nfailor@gmail.com>
2024-02-23 21:01:43 -0600

    Updated post query and auto-update page when new post is made

--- client/src/components/Posts/PostContainer.jsx
+++ client/src/components/Posts/PostContainer.jsx
@@ -3,8 +3,18 @@ import SubmitPosts from "./submitPost";
 //Add Map For Map.Posts
 import AuthService from "../../utils/auth";
 import { QUERY_POST } from "../../utils/queries";
+import { useQuery } from "@apollo/client";
 
 const PostContainer = () => {
+
+  const { loading, error, data } = useQuery(QUERY_POST);
+
+  const posts = data?.posts || [];
+
+  
+
+
+
 	return AuthService.loggedIn() ? (
 		<div
 			class="w-postContainer overflow-y-auto flex flex-col items-center shadow-custom bg-gray-dark h-postContainer rounded-custom"
@@ -17,7 +27,7 @@ const PostContainer = () => {
 		>
 			<SubmitPosts />
 			<div class="overflow-y-auto mt-5">
-				<Post />
+				<Post posts={posts} />
 			</div>
 		</div>
 	) : (
--- client/src/components/Posts/submitPost.jsx
+++ client/src/components/Posts/submitPost.jsx
@@ -2,67 +2,67 @@ import { CREATE_POST } from "../../utils/mutation";
 import React, { useState } from "react";
 import { useMutation } from "@apollo/client";
 import AuthService from "../../utils/auth";
-
-
-
-
-
+import { QUERY_POST } from "../../utils/queries";
 
 const SubmitPosts = ({ posts }) => {
+	const [postValue, setPostValue] = useState("");
 
-  const [postValue, setPostValue] = useState('');
+	const [createPost, { error }] = useMutation(CREATE_POST, {
+		refetchQueries: [QUERY_POST, "posts"],
+	});
 
-  const [createPost, { error }] = useMutation(CREATE_POST);
+	const handleInputChange = (event) => {
+		const postText = event.target.value;
+		setPostValue(postText);
+	};
 
-  const handleInputChange = (event) => {
-    const postText = event.target.value;
-    setPostValue(postText);
-  };
+	const handleFormSubmit = async (event) => {
+		event.preventDefault();
 
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-
-    try {
-      const authUser = AuthService.getProfile();
+		try {
+			const authUser = AuthService.getProfile();
 			if (!authUser) {
 				console.error("User not authenticated");
 				return;
 			}
 
-      const { data } = await createPost({
+			const { data } = await createPost({
 				variables: {
 					postText: postValue,
 					postAuthor: authUser.name, // Adjust based on your token payload
 				},
 			});
 
-      setPostValue('');
-      
-    } catch (err) {
-      console.error(err);
-    }
-  };
+			setPostValue("");
+		} catch (err) {
+			console.error(err);
+		}
+	};
 
-
-  return (
-    <div class=" flex-shrink h-submitPost w-submitPost mt-5 font-custom  shadow-custom box-border flex flex-wrap items-center justify-center flex-col rounded-custom bg-gray">
-    <input value={postValue} onChange={handleInputChange} class="font-extralight text-white text-2xl shadow-inner-strongest text-center w-inputSubmitPost h-inputSubmitPost  bg-gray-light rounded-custom" placeholder="Share your thoughts...">
-
-    </input>
-    <div class="bg-black w-2/3 mt-3 h-line"></div>
-   <div class=" flex-shrink w-inputSubmitPost h-12 mt-4  flex flex-wrap justify-evenly items-center text-white">
-     <div class=" flex justify-around w-5/12">
-     <button class=" w-16 h-6 text-lg ">Photo</button>
-     </div>
-     <div class="flex w-5/12 justify-end">
-            <button onClick={handleFormSubmit} class= " w-32 text-lg rounded-custom bg-button-dark hover:bg-slate-600">Send</button>
-    </div>
-  </div>
-  </div>
-  );
+	return (
+		<div class=" flex-shrink h-submitPost w-submitPost mt-5 font-custom  shadow-custom box-border flex flex-wrap items-center justify-center flex-col rounded-custom bg-gray">
+			<input
+				value={postValue}
+				onChange={handleInputChange}
+				class="font-extralight text-white text-2xl shadow-inner-strongest text-center w-inputSubmitPost h-inputSubmitPost  bg-gray-light rounded-custom"
+				placeholder="Share your thoughts..."
+			></input>
+			<div class="bg-black w-2/3 mt-3 h-line"></div>
+			<div class=" flex-shrink w-inputSubmitPost h-12 mt-4  flex flex-wrap justify-evenly items-center text-white">
+				<div class=" flex justify-around w-5/12">
+					<button class=" w-16 h-6 text-lg ">Photo</button>
+				</div>
+				<div class="flex w-5/12 justify-end">
+					<button
+						onClick={handleFormSubmit}
+						class=" w-32 text-lg rounded-custom bg-button-dark hover:bg-slate-600"
+					>
+						Send
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 };
-
-
-
 
 export default SubmitPosts;
