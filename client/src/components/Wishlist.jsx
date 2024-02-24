3fa3ead49af43136749a29c261fee4b2cb61ab94
Terrence McCarthy <terrencemccarthysnkrs@gmail.com>
2024-02-23 21:11:08 -0600

    working in wishlist

--- client/src/components/Wishlist.jsx
+++ client/src/components/Wishlist.jsx
@@ -2,15 +2,45 @@ import React, { useState } from "react";
 import { useMutation } from '@apollo/client';
 import { useParams } from 'react-router-dom';
 
+import { CREATE_TRIP } from "../utils/mutation";
+
 const Wishlist = ({
-  wishlist
+	wishlist
 }) => {
-  if (!wishlist.length) {
-    return <h3>No Wishlist Yet</h3>
-  }
+	//   if (!wishlist.length) {
+	//     return <h3>No Wishlist Yet</h3>
+	//   }
 
+	const [showInputBox, setShowInputBox] = useState(false);
+	const [inputState, setInputState] = useState({
+		name: '',
+	});
 
+	const [createTrip, { error, data }] = useMutation(CREATE_TRIP);
 
+	const handleInputChange = (event) => {
+		const { name, value } = event.target;
+
+		setInputState({
+			...inputState,
+			[name]: value,
+		});
+	};
+
+	const handleInputSubmit = async (event) => {
+		event.preventDefault();
+
+		try {
+			const { data } = await createTrip({
+				variables: { ...inputState }
+			});
+
+		} catch (error) {
+			console.error(error)
+		}
+	}
+
+	
 	return (
 		<div class="box-border flex h-wishlist-height w-wishlist-width flex-wrap items-center justify-center rounded-custom bg-gray shadow-2xl">
 			<div class="text-2xl font-semibold text-white">
@@ -18,14 +48,14 @@ const Wishlist = ({
 			</div>
 
 			<div class="mt-2 box-border flex h-inner-wishlist-height w-inner-wishlist-width flex-col items-center justify-start rounded-custom bg-gray-dark p-4 shadow-inner-strong">
-        {wishlist &&
-          wishlist.map((tripinfo) => (
-            <button key={tripinfo._id} class="mb-5 flex h-10 w-40 items-center justify-center rounded-custom bg-green-200">
-            <p class="font-semibold text-black">{tripinfo.name}</p>
-            </button>
-          ))
-        }
-					{/* We will need to add with the Queried Data to this area here and when the button is produced, it should then trigger the Modal to start the Itinerary */}
+				{wishlist &&
+					wishlist.map((tripinfo) => (
+						<button key={tripinfo._id} class="mb-5 flex h-10 w-40 items-center justify-center rounded-custom bg-green-200">
+							<p class="font-semibold text-black">{tripinfo.name}</p>
+						</button>
+					))
+				}
+				{/* We will need to add with the Queried Data to this area here and when the button is produced, it should then trigger the Modal to start the Itinerary */}
 			</div>
 			<div class="flex items-center justify-evenly">
 				<button class="h-4 w-4 rounded-full bg-black"></button>
