43f60995361c880f2481db87f100dafaecb84b0d
Chad Edwards <chadedwardsofficial@gmail.com>
2024-02-28 16:09:38 -0600

    styled the searchBar

--- client/src/components/Navbar/SearchBar.jsx
+++ client/src/components/Navbar/SearchBar.jsx
@@ -3,7 +3,7 @@ import { QUERY_SINGLE_PROFILE } from "../../utils/queries";
 import { QUERY_PROFILE_BY_NAME } from "../../utils/queries"; 
 import { useMutation, useQuery, useLazyQuery } from "@apollo/client"; 
 import { Link } from "react-router-dom";
-
+import { useTheme } from "../../utils/ThemeContext";
 const SearchBar = () => {
   const [searchInput, setSearchInput] = useState('')
   const [profileId, setProfileId] = useState("");
@@ -54,11 +54,16 @@ const SearchBar = () => {
     
 
   };
+  const [themeState] = useTheme();
+  const navbarStyles = {
+    background: themeState.darkTheme ? 'white' : 'gray',
+		color: themeState.darkTheme ? 'black' : 'white',
+  };
 
   return (
     <>
     <form  className="flex flex-row">
-      <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white" >
+      <label htmlFor="default-search" className="mb-2 text-sm font-medium sr-only dark:text-white" >
         Search
       </label>
       <div className="flex justify-center items-center">
@@ -66,14 +71,16 @@ const SearchBar = () => {
           type="text"
           onChange={handleInputChange}
           id="default-search"
-          className="w-full  text-md text-white border border-black rounded-custom px-4 py-1 bg-gray-light "
+          className="w-full  text-md text-white border border-black rounded-custom px-4 py-1 "
+          style={navbarStyles}
           placeholder="Search SkySocial"
           required
         />
         <button
           type="submit"
           onClick={handleFormSubmit}
-          className="text-white ml-3 h-7 border-black bg-gray-light rounded-custom w-32"
+          className=" border ml-3 h-7 border-black rounded-custom w-32"
+          style={navbarStyles}
         >
           Search
         </button>
@@ -84,7 +91,7 @@ const SearchBar = () => {
 
 
   {modalOpen && (
-        <div className="absolute flex justify-between items-center w-52 mt-2 bg-gray-light rounded-custom border border-black ml-3 text-white">
+        <div className="absolute flex justify-between items-center w-52 mt-2  rounded-custom border border-black ml-3 " style={navbarStyles}>
           <Link className="ml-3" to={`/profiles/${profileId}`}>{profileName}</Link>
           <button className="text-md mr-2 w-4 " onClick={onHide}>
             x
